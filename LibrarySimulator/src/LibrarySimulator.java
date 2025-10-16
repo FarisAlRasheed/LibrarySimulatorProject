9d21218ac9554cbe98575843020db0991229c545
Faris AlRasheed <farsalrasheed27@gmail.com>
2025-10-16 12:29:23 +0300

    test commit

---
+++ LibrarySimulator/src/LibrarySimulator.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class LibrarySimulator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Hello, World!, Enter your name: ");
+        String name = in.next();
+        System.out.println("Hi! "+name);
+    }
+}
