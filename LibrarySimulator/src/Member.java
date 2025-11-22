fdb0396235deaaa9ba2fc0af58fd1b9c9ea20c64
FSD75 <fahdsaleh68@gmail.com>
2025-11-22 11:32:36 +0300

    Add methods

--- LibrarySimulator/src/Member.java
+++ LibrarySimulator/src/Member.java
@@ -8,67 +8,93 @@ public class Member {
 	private int numBorrows;
 	private int numReturns;
 	private double sessionFees;
-
-	//admin
+	//
 	public static double totalRevenue;
 	public static int totalViewBorrowed;
 	public static int totalBorrows;
 	public static int totalReturns;
-	
+
 	//
-	public Member(int id,String name, int borrowedCount) {
-		this.id=id;
-		this.name=name;
-		this.borrowedCount=borrowedCount;
+	public Member(int id, String name, int borrowedCount) {
+		this.id = id;
+		this.name = name;
+		this.borrowedCount = borrowedCount;
 
 	}
 //
-	public Member() {
-	id=0;
-	name="";
-	borrowedCount=0;
-		
+
+	public void displayStatistics() {
+		System.out.println("\nSession Activity Summary");
+		System.out.println("===\nBooks Borrowed: " + numBorrows);
+		System.out.println("Books Returned: " + numReturns);
+		System.out.println("Total Fees: " + sessionFees + "\n===");
+
 	}
 
 	private boolean canBorrow() {
-		System.out.println("\n ## You can't borrow more than 5 books at once! ## ");
-		return borrowedCount<5;
-	}
-		
-	private boolean canReturn() {
-		System.out.println("\n## You don't have books to return! ##");
-		return borrowedCount>0;		
+
+		return borrowedCount < 5;
 	}
 
-	private  void viewBorrowedCount() {
-		System.out.println("===\nBooks Borrowed: " + numBorrows);
+	private boolean canReturn() {
+
+		return borrowedCount > 0;
+
 	}
-	
-	private void borrowOne() {
-		if (canBorrow() == false) {
-			return;
-		}
-		else {
-			numBorrows++;
+
+	public boolean borrowOne() {
+
+		if (canBorrow()) {
+			borrowedCount++;
 			totalBorrows++;
+			numBorrows++;
 			sessionFees += 0.5;
 			totalRevenue += 0.5;
+			return true;
+		} else {
+			System.err.println("\n ## You can't borrow more than 5 books at once! ## ");
+		return false;
 		}
-		
+
 	}
-	private void returnOne() {
-		if(canReturn() == false) {
-			return;
-		}
-		else{
-			numBorrows--;
+
+	public void reset() {
+		numBorrows = 0;
+		numReturns = 0;
+		sessionFees = 0;
+	}
+
+	public boolean returnOne() {
+
+		if (canReturn()) {
 			numReturns++;
-			totalBorrows--;
 			totalReturns++;
+
+			borrowedCount--;
+return true;
+		} else {
+			System.err.println("\n## You don't have books to return! ##");
+		return false;
 		}
+
 	}
-	
-	
-	
+
+	public String getName() {
+
+		return name;
+	}
+
+	public int getId() {
+
+		return id;
+	}
+
+
+	public int viewBorrowedCount() {
+
+		numViewBorrowed++;
+		return borrowedCount;
+	}
+
 	
 }
