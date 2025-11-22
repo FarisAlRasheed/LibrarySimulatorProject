327b12d160378dcc1295cafe72c0c96300f46e05
Faris AlRasheed <101565358+FarisAlRasheed@users.noreply.github.com>
2025-11-22 12:22:16 +0300

    Update Member.java

--- LibrarySimulator/src/Member.java
+++ LibrarySimulator/src/Member.java
@@ -1,100 +1,126 @@
+/*
+	Team members:
+	Name 							Uni ID
+	1-Fahad Aldeghaither			446102355
+	2-Faris Alrasheed  				446101056
+	3-Abdulrahman Alwaalan			446107578
 
+
+   GitHub repositery URL: https://github.com/FarisAlRasheed/LibrarySimulatorProject/tree/Member
+ */
 public class Member {
-	//
+	//instances attributes
 	private int id;
 	private String name;
-	private int borrowedCount;
+	private int borrowedCount;//currently borrowed books for each user
 	private int numViewBorrowed;
 	private int numBorrows;
 	private int numReturns;
 	private double sessionFees;
-	//
+	//static vars across all instances
 	public static double totalRevenue;
 	public static int totalViewBorrowed;
 	public static int totalBorrows;
 	public static int totalReturns;
-
-	//
-	public Member(int id, String name, int borrowedCount) {
-		this.id = id;
-		this.name = name;
-		this.borrowedCount = borrowedCount;
-
+	
+	//main constructor
+	public Member(int id,String name, int borrowedCount) {
+		this.id=id;
+		this.name=name;
+		this.borrowedCount=borrowedCount;
 	}
-//
+	//default constructor
+	public Member() {
+		id=0;
+		name="";
+		borrowedCount=0;
+	}
+
+	//if can borrow condition
+	private boolean canBorrow() {
+		
+		return borrowedCount<5;
+	}
+	
+	//if can return condition
+	private boolean canReturn() {
+		
+			return borrowedCount>0;		
+		
+	}
+	
+	public boolean borrowOne() {
+		if (canBorrow()) {
+			borrowedCount++;
+			numBorrows++;
+			totalBorrows++;
+			sessionFees += 0.5;
+			totalRevenue += 0.5;
+			return true;
+		}
+		else {
+			System.err.println("\nERROR: You can't borrow more than 5 books at once!");
+			return false;
+		}
+		
+	}
+	public boolean returnOne() {
+		if(canReturn()) {
+			borrowedCount--;
+			numReturns++;
+			totalBorrows--;
+			totalReturns++;
+			return true;
+		}
+		else{
+			System.err.println("\nERROR: You don't have books to return!");
+			return false;
+		}
+	}
+	
 
 	public void displayStatistics() {
 		System.out.println("\nSession Activity Summary");
 		System.out.println("===\nBooks Borrowed: " + numBorrows);
 		System.out.println("Books Returned: " + numReturns);
-		System.out.println("Total Fees: " + sessionFees + "\n===");
-
+		System.out.printf("Total Fees: %.2f\n===\n", sessionFees);
 	}
-
-	private boolean canBorrow() {
-
-		return borrowedCount < 5;
-	}
-
-	private boolean canReturn() {
-
-		return borrowedCount > 0;
-
-	}
-
-	public boolean borrowOne() {
-
-		if (canBorrow()) {
-			borrowedCount++;
-			totalBorrows++;
-			numBorrows++;
-			sessionFees += 0.5;
-			totalRevenue += 0.5;
-			return true;
-		} else {
-			System.err.println("\n ## You can't borrow more than 5 books at once! ## ");
-		return false;
-		}
-
-	}
-
+	
 	public void reset() {
-		numBorrows = 0;
-		numReturns = 0;
 		sessionFees = 0;
+		numReturns = 0;
+		numBorrows = 0;
 	}
-
-	public boolean returnOne() {
-
-		if (canReturn()) {
-			numReturns++;
-			totalReturns++;
-
-			borrowedCount--;
-return true;
-		} else {
-			System.err.println("\n## You don't have books to return! ##");
-		return false;
-		}
-
-	}
-
+	
 	public String getName() {
-
 		return name;
 	}
-
-	public int getId() {
-
-		return id;
+	
+	public double getSessionFees() {
+		return sessionFees;
 	}
-
-
-	public int viewBorrowedCount() {
-
-		numViewBorrowed++;
+	public int getBorrowed() {
 		return borrowedCount;
 	}
-
+	public static void frequentOperation() {
+		System.out.println("\nMost Frequent Operations");
+		System.out.println("===");
+		if(totalBorrows == totalReturns){
+			System.out.println("borrow and return opperations are the same");
+		}
+		else if(totalBorrows > totalReturns){
+			System.out.println("borrow opperations are more frequent");
+		}
+		else{
+			System.out.println("return opperations are more frequent");
+		}
+		System.out.println("===");
+		
+	}
+	
+	public void viewBorrowedCount() {
+		System.out.println("Borrowed Count = " + borrowedCount);
+		numViewBorrowed++;
+	}
 	
 }
